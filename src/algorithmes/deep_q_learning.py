87d93e06885fc09198ea3068cf63acb576b77ad6
Stooms21 <touzet.stan@protonmail.com>
2024-10-03 00:08:31 +0200

    ajout commentaires

--- src/algorithmes/deep_q_learning.py
+++ src/algorithmes/deep_q_learning.py
@@ -14,47 +14,62 @@ def deep_q_learning(
         gamma: float = 0.999,
         nb_episode: int = 1000,
 ):
-    # Initialize Q(s,a) arbitrarily
+    """
+    Fonction d'apprentissage par Q-learning profond. Elle retourne le réseau de neurone entrainé.
+    Ce réseau de neurone est ensuite utilisé pour estimer la meilleure action à prendre à chaque état.
+    """
+
+    # Initialiser Q(s,a) de manière arbitraire
     input_layer_size = env.get_one_hot_size()
     output_layer_size = env.num_actions()
     policy_network = models.QNet(input_layer_size, output_layer_size, DQN_HIDDEN_LAYER_SIZE)
 
-    # Loop for each episode
+    # Boucle pour chaque épisode
     for _ in tqdm.tqdm(range(nb_episode)):
-        #Initialize S
+        # Initialiser S
         env.reset()
-        # Loop for each step epiosde
+        # Boucle pour chaque étape de l'épisode
         while not env.is_game_over():
             s = env.one_hot_state_desc()
             available_actions = env.available_actions()
 
-            # Choose A from S using policy derived from Q
+            # Choisir A à partir de S en utilisant la politique dérivée de Q
             a = dqu.choose_epsilon_greedy_action(policy_network, s, available_actions, epsilon)
 
-            # Take action A, observe R, S'
+            # Prendre l'action A, observer R, S'
             reward, s_prime, available_actions_prime = dqu.observe_R_S_prime(env, a)
 
-            # Compute Q(s,a) and Q_target
+            # Calculer Q(s,a) et Q_target
             q_value, q_target = dqu.compute_q_values_and_q_target(env, policy_network, s, s_prime, a, gamma, reward)
 
-            # Update Q(s,a)
+            # Mettre à jour Q(s,a)
             policy_network.backward(q_value, q_target)
 
-        # epsilon decay
+        # Décroissance de epsilon
         epsilon = max(epsilon_min, epsilon * epsilon_decay)
 
     return policy_network
 
 
 if __name__ == "__main__":
-    env_name = "GridWorld"
+    # Nom de l'environnement
+    env_name = "LineWorld"
+    # Charger la configuration de l'environnement
     config = ut.load_config(CONFIG_FILE, env_name)
+    # Obtenir le module de l'environnement
     env_module = ENV_MODULE_MAPPING[env_name]
+    # Obtenir la classe de l'environnement
     env_class = getattr(env_module, env_name)
+    # Initialiser l'environnement avec la configuration
     env = env_class(config)
     reward = 0
+    # Boucle pour jouer 10 parties
     for i in range(1, 11):
+        # Réinitialiser l'environnement
         env.reset()
+        # Appliquer l'apprentissage par Q-learning profond
         policy_network = deep_q_learning(env)
+        # Jouer une partie avec le réseau de politique appris
         reward += env.play(policy_network)
+        # Afficher la récompense moyenne sur les parties jouées
         print(f"Reward moyen: {reward / i} sur {i} parties")
--- src/algorithmes/double_deep_q_learning.py
+++ src/algorithmes/double_deep_q_learning.py
@@ -15,54 +15,69 @@ def deep_q_learning(
         nb_episode: int = 1000,
         nb_target_update: int = 10
 ):
-    # Initialize Q(s,a) arbitrarily
+    """
+    Fonction d'apprentissage par Q-learning profond avec double réseau de neurones. 
+    Elle retourne le réseau de neurone de politique entrainé.
+    """
+
+    # Initialiser Q(s,a) de manière arbitraire
     input_layer_size = env.get_one_hot_size()
     output_layer_size = env.num_actions()
     policy_network = models.QNet(input_layer_size, output_layer_size, DDQN_HIDDEN_LAYER_SIZE)
 
-    # Initialize the target network
+    # Initialiser le réseau cible
     target_network = models.QNet(input_layer_size, output_layer_size, DDQN_HIDDEN_LAYER_SIZE)
     target_network.load_state_dict(policy_network.state_dict())
 
-    # Loop for each episode
+    # Boucle pour chaque épisode
     for _ in tqdm.tqdm(range(nb_episode)):
-        # Initialize S
+        # Initialiser S
         env.reset()
-        # Copy the policy network to the target network nb_target_update times
-        if _ % (nb_episode//nb_target_update) == 0:
+        # Copier le réseau de politique dans le réseau cible nb_target_update fois
+        if _ % (nb_episode // nb_target_update) == 0:
             target_network.load_state_dict(policy_network.state_dict())
 
-        # Loop for each step episode
+        # Boucle pour chaque étape de l'épisode
         while not env.is_game_over():
             s = env.one_hot_state_desc()
             available_actions = env.available_actions()
 
-            # Choose A from S using policy derived from Q
+            # Choisir A à partir de S en utilisant la politique dérivée de Q
             a = dqu.choose_epsilon_greedy_action(policy_network, s, available_actions, epsilon)
 
-            # Take action A, observe R, S'
+            # Prendre l'action A, observer R, S'
             reward, s_prime, available_actions_prime = dqu.observe_R_S_prime(env, a)
 
-            # Compute Q(s,a) and Q_target
+            # Calculer Q(s,a) et Q_target
             q_value, q_target = dqu.compute_q_values_and_q_target(env, policy_network, s, s_prime, a, gamma, reward, target_network)
 
-            # Update Q(s,a)
+            # Mettre à jour Q(s,a)
             policy_network.backward(q_value, q_target)
 
-        # epsilon decay
+        # Décroissance de epsilon
         epsilon = max(epsilon_min, epsilon * epsilon_decay)
 
     return policy_network
 
 if __name__ == "__main__":
-    env_name = "GridWorld"
+    # Nom de l'environnement
+    env_name = "LineWorld"
+    # Charger la configuration de l'environnement
     config = ut.load_config(CONFIG_FILE, env_name)
+    # Obtenir le module de l'environnement
     env_module = ENV_MODULE_MAPPING[env_name]
+    # Obtenir la classe de l'environnement
     env_class = getattr(env_module, env_name)
+    # Initialiser l'environnement avec la configuration
     env = env_class(config)
     reward = 0
+    # Boucle pour jouer 10 parties
     for i in range(1, 11):
+        # Réinitialiser l'environnement
         env.reset()
+        # Appliquer l'apprentissage par Q-learning profond
         policy_network = deep_q_learning(env)
+        # Jouer une partie avec le réseau de politique appris
         reward += env.play(policy_network)
+        # Afficher la récompense moyenne sur les parties jouées
         print(f"Reward moyen: {reward / i} sur {i} parties")
